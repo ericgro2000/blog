30aa98c30c116c5332da6582727b5f17b5daf5ab
ericgro2000 <eric.gro.2000@gmail.com>
2024-08-27 16:55:11 +0200

    .

--- src/app/posts/loading.tsx
+++ src/app/posts/loading.tsx
@@ -4,7 +4,7 @@ import { SkeletonList, SkeletonPostCard } from "../Sleketons/Skeleton";
 export default function LoadingPostsPage() {
   return (
     <>
-      <h1 className="page-title">Posts</h1>
+
 
       <div className="card-grid">
         <SkeletonList amount={6}>
